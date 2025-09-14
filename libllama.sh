4c1a5f34e8f63a35b3310fe19732ade5f369faac
Victor Carré <victorcarre6@gmail.com>
2025-09-14 19:27:06 +0200

    Delete libllama.sh

--- libllama.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-git clone https://github.com/ggml-org/llama.cpp.git || echo "Repo déjà cloné"
-cd llama.cpp || exit
-cmake -B build -S . -DLLAMA_CUBLAS=OFF -DCMAKE_BUILD_TYPE=Release
-cmake --build build
-echo "Compilation terminée. LIBLLAMA sera : $(pwd)/build/libllama.dylib"
-ls build/libllama.dylib
-export LIBLLAMA=/Users/victorcarre/Code/Projects/llm-assistant/resources/models/llama.cpp/build/libllama.dylib
